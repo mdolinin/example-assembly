60708a5ba0faace2100c571600d72baf9cf71610
mdolinin <dmo.builder@gmail.com>
2017-12-06 22:12:33 +0200

    set minimesos env vars

--- project/script/delete-app.sh
+++ project/script/delete-app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
 
-curl -X DELETE "${MINIMESOS_MARATHON}/v2/apps/web-app"
+curl -X DELETE "${MINIMESOS_MARATHON%;}/v2/apps/web-app"
--- project/script/deploy-app.sh
+++ project/script/deploy-app.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -ex
 
-curl -X POST "${MINIMESOS_MARATHON}/v2/apps" -d @./project/script/marathon-web-app.json -H "Content-type: application/json"
+$(minimesos info | grep MINIMESOS)
+
+curl -X POST "${MINIMESOS_MARATHON%;}/v2/apps" -d @./project/script/marathon-web-app.json -H "Content-type: application/json"
